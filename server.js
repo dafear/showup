f4bbc825e072ae25d2361ff3a7db583cba5d789f
sme_admin <dhafir.fuller@gmail.com>
2017-05-13 22:02:05 -0400

    new and fixed

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express();
+var path = require('path');
 
 app.use(express.static('public'));
-
+///app starting points are verbs like get, post, delete, put, patch updates what was there//
+app.get('/search' ,function (req, res) {
+ console.log('hello',req.query.q)
+ res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 app.listen(process.env.PORT || 8080);
 
 exports.app = app;
